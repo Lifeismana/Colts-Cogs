b1fff5f79e4d0e3c56bfcfa3e0e68445d02aaa90
Antoine Rybacki <antoine.rybacki@etu.univ-lyon1.fr>
2020-11-28 14:22:42 +0100

    am i shit ?

--- speedtest/speedtest.py
+++ speedtest/speedtest.py
@@ -47,7 +47,7 @@ class Speedtest(commands.Cog):
             color=await ctx.embed_colour(), title="Running speedtest ... This may take a while! ⏱",
         )
         msg = await ctx.send(embed=em)
-        results = await self.bot.loop.run_in_executor(None, self._speedtest,"--accept-license --accept-gdpr")
+        results = await self.bot.loop.run_in_executor(None, self._speedtest,"--accept-license", "--accept-gdpr")
         if results[1]:
             em.color = discord.Color.dark_red()
             em.title = "Failed to get a speedtest result."
